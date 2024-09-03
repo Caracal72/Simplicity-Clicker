3becfc6f19da470b308fd95de3209a48f0194d14
gamerdj325 <81705106+gamerdj325@users.noreply.github.com>
2024-09-03 00:25:23 -0400

    Changed window name and made button anchor in center

--- stats/main.py
+++ stats/main.py
@@ -9,17 +9,17 @@ def on_button_click(command=lambda: on_button_click(button)):
     print(points)
 # Create the main window
 root = tk.Tk()
-root.title("Button Example")
+root.title("Simplicity Clicker")
 
 # Create a button widget
-button = tk.Button(root, text="I love you", command=on_button_click)
+button = tk.Button(root, text="I love you", width=5, height=3, command=on_button_click)
 
 button.config(command=lambda: on_button_click(button))
 
 button.config(text=points)
 
 # Place the button on the window
-button.pack()
+button.pack(expand=True, anchor="center")
 
 # Run the application
 root.mainloop()
