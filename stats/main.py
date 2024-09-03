261547be037c841f908c8775782d01a8111dbc2d
Gamerdj325 <Gamerdj325@gmail.com>
2024-09-03 01:24:26 -0400

    Made Background Black | added black background | Added shop Button | made min window size |

--- stats/main.py
+++ stats/main.py
@@ -2,24 +2,34 @@ import tkinter as tk
 
 points = 0
 
-def on_button_click(command=lambda: on_button_click(button)):
+def on_button_click(button):
     global points
     points += 1
     button.config(text=str(points))
-    print(points)
 # Create the main window
 root = tk.Tk()
+root.minsize(300, 200)
 root.title("Simplicity Clicker")
+root.config(bg="black")
 
-# Create a button widget
-button = tk.Button(root, text="I love you", width=5, height=3, command=on_button_click)
+# Create a buttons (shop and the clicker button)
+button = tk.Button(root, text="I love you", width=10, height=5, command=lambda: on_button_click (button))
 
-button.config(command=lambda: on_button_click(button))
+button.config()
 
 button.config(text=points)
 
+shop = tk.Button(root, text="shop", width=8, height=4, command=lambda: shop_click(shop))
+
+shop.config()
+
+def shop_click(shop):
+    print("shop open sequence")
+
 # Place the button on the window
 button.pack(expand=True, anchor="center")
+shop.pack()
+shop.place(x=5, y=5)
 
 # Run the application
 root.mainloop()
